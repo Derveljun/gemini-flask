10f931f23295a668d818e91d76039edf91d0e213
JUNHO_YE <junho_ye@daekyo.co.kr>
2022-03-22 18:52:20 +0900

    #fix: bat file update

--- gemini-flask.bat
+++ gemini-flask.bat
@@ -2,10 +2,9 @@
 d:
 cd D:\Repos\github\gemini-flask
 
+:: 실행
+cd D:\Repos\github\gemini-flask\venvs\gemini-flask-venv\Scripts\activate.bat
+
 :: pybo.py 또는 pybo 폴더의 __init__.py를 찾음
 set FLASK_APP=pybo 
-
 set FLASK_ENV=development
-
-:: 실행
-d:\Repos\github\gemini-flask\venvs\gemini-flask-venv\Scripts\activate
